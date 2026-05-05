5bf8e2321507f719fdd8f0b4cf7639e286d94ea9
Saurav Wahid <wahid.saurav@gmail.com>
2026-05-05 23:34:07 +0600

    add capacitor and update ios files

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.furepet.app',
+  appName: 'FurePet',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
